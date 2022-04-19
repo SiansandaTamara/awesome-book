e54f8f8034daa3b17929afd12f60cb3875cefbf0
Siansanda Tamara <tamarasiansanda@gmail.com>
2022-04-19 13:11:13 +0200

    Modify files for linters

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
-
-   
 const findHight = () => {
-    if (window.innerHeight < document.body.scrollHeight) {
-      document.querySelector('footer').style.position = 'relative';
-    } else {
-      document.querySelector('footer').style.position = 'fixed';
-    }
-  };
-  
-  class Books {
-    constructor(title, author) {
-      this.title = title;
-      this.author = author;
-    }
-  
+  if (window.innerHeight < document.body.scrollHeight) {
+    document.querySelector('footer').style.position = 'relative';
+  } else {
+    document.querySelector('footer').style.position = 'fixed';
+  }
+};
+
+class Books {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
+
     static db = () => ((window.localStorage.getItem('books') !== null) ? JSON.parse(window.localStorage.getItem('books')) : [])
-  
+
     static removeBook = (button) => {
       const result = this.db().filter((word) => word.title !== button.getAttribute('data-book-name'));
       button.parentElement.remove();
@@ -23,7 +21,7 @@ const findHight = () => {
       this.showAlert('Book deleted ', 'success');
       findHight();
     }
-  
+
     static load = () => {
       this.db().forEach((data) => {
         document.querySelector('#listbooks').innerHTML += `
@@ -34,7 +32,7 @@ const findHight = () => {
               `;
       });
     }
-  
+
      static addBook = (book) => {
        const result = this.db().filter((word) => word.title === book.title);
        if (result.length === 0) {
@@ -56,7 +54,7 @@ const findHight = () => {
          this.showAlert('Book already exists ', 'danger');
        }
      }
-  
+
      static showAlert(message, className) {
        const div = document.createElement('div');
        div.className = `alert ${className}`;
@@ -66,73 +64,73 @@ const findHight = () => {
        container.insertBefore(div, formtitle);
        setTimeout(() => document.querySelector('.alert').remove(), 3000);
      }
+}
+
+const returnSufixDate = (digit) => {
+  const toArray = digit.toString().split('');
+  const toCheck = parseInt(toArray[toArray.length - 1], 10);
+  switch (true) {
+    case toCheck === 1:
+      return 'st';
+    case toCheck === 2:
+      return 'nd';
+    case toCheck === 3:
+      return 'rd';
+    default:
+      return 'th';
   }
-  
-  const returnSufixDate = (digit) => {
-    const toArray = digit.toString().split('');
-    const toCheck = parseInt(toArray[toArray.length - 1], 10);
-    switch (true) {
-      case toCheck === 1:
-        return 'st';
-      case toCheck === 2:
-        return 'nd';
-      case toCheck === 3:
-        return 'rd';
-      default:
-        return 'th';
-    }
-  };
-  
-  const formatDate = () => {
-    const CurrentTime = new Date();
-    const currentHour = CurrentTime.getHours();
-    let newHour;
-    const suffix = (currentHour >= 12 && currentHour !== 0) ? 'pm' : 'am';
-    if (currentHour > 12) {
-      newHour = currentHour - 12;
-    } else if (currentHour === 0) {
-      newHour = 12;
-    } else {
-      newHour = currentHour;
-    }
-    return `${newHour}:${CurrentTime.getMinutes()}:${CurrentTime.getSeconds()} ${suffix}`;
-  };
-  
-  (() => {
-    Books.load();
-    const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    document.querySelector('#todaydate').innerHTML = `${monthNames[new Date().getMonth()]} ${new Date().getDate()}<sup>${returnSufixDate(new Date().getDate())}</sup> ${new Date().getFullYear()}`;
-    setInterval(() => {
-      document.querySelector('#currenttime').innerHTML = `${formatDate()}`;
-    }, 1000);
-    findHight();
-  })();
-  
-  document.querySelector('#addbookform').addEventListener('submit', (e) => {
+};
+
+const formatDate = () => {
+  const CurrentTime = new Date();
+  const currentHour = CurrentTime.getHours();
+  let newHour;
+  const suffix = (currentHour >= 12 && currentHour !== 0) ? 'pm' : 'am';
+  if (currentHour > 12) {
+    newHour = currentHour - 12;
+  } else if (currentHour === 0) {
+    newHour = 12;
+  } else {
+    newHour = currentHour;
+  }
+  return `${newHour}:${CurrentTime.getMinutes()}:${CurrentTime.getSeconds()} ${suffix}`;
+};
+
+(() => {
+  Books.load();
+  const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  document.querySelector('#todaydate').innerHTML = `${monthNames[new Date().getMonth()]} ${new Date().getDate()}<sup>${returnSufixDate(new Date().getDate())}</sup> ${new Date().getFullYear()}`;
+  setInterval(() => {
+    document.querySelector('#currenttime').innerHTML = `${formatDate()}`;
+  }, 1000);
+  findHight();
+})();
+
+document.querySelector('#addbookform').addEventListener('submit', (e) => {
+  e.preventDefault();
+  const title = document.querySelector('#title');
+  const author = document.querySelector('#author');
+  if (title.value !== '' && author.value !== '') {
+    const BookObj = new Books(title.value, author.value);
+    Books.addBook(BookObj);
+    title.value = '';
+    author.value = '';
+  } else {
+    Books.showAlert('Please fill all the fields', 'danger');
+  }
+});
+
+document.querySelectorAll('.removeBook').forEach((deleteBook) => {
+  deleteBook.addEventListener('click', () => Books.removeBook(deleteBook));
+});
+
+document.querySelectorAll('.nav').forEach((nav) => {
+  nav.addEventListener('click', (e) => {
+    document.querySelectorAll('.nav').forEach((nav2) => nav2.classList.remove('activelink'));
     e.preventDefault();
-    const title = document.querySelector('#title');
-    const author = document.querySelector('#author');
-    if (title.value !== '' && author.value !== '') {
-      const BookObj = new Books(title.value, author.value);
-      Books.addBook(BookObj);
-      title.value = '';
-      author.value = '';
-    } else {
-      Books.showAlert('Please fill all the fields', 'danger');
-    }
+    document.querySelectorAll('.tab').forEach((tab) => tab.classList.remove('active'));
+    e.target.classList.add('activelink');
+    document.querySelector(e.target.getAttribute('href')).classList.add('active');
+    findHight();
   });
-  
-  document.querySelectorAll('.removeBook').forEach((deleteBook) => {
-    deleteBook.addEventListener('click', () => Books.removeBook(deleteBook));
-  });
-  
-  document.querySelectorAll('.nav').forEach((nav) => {
-    nav.addEventListener('click', (e) => {
-      document.querySelectorAll('.nav').forEach((nav2) => nav2.classList.remove('activelink'));
-      e.preventDefault();
-      document.querySelectorAll('.tab').forEach((tab) => tab.classList.remove('active'));
-      e.target.classList.add('activelink');
-      document.querySelector(e.target.getAttribute('href')).classList.add('active');
-      findHight();
-    });
-  });
+});
